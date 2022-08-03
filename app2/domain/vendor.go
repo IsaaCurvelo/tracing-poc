4e1e67753c4e1af5498b9b79a4ef8df9ae3748e6
Luan <luan.santos@bexsbanco.com.br>
2022-08-03 18:03:07 -0300

    added exclusive_titles in domain layer

--- app2/domain/vendor.go
+++ app2/domain/vendor.go
@@ -1,7 +1,14 @@
 package domain
 
+type ExclusiveTitle struct {
+	ID       string
+	Name     string
+	VendorID string
+}
+
 type Vendor struct {
 	ID            string
 	Name          string
 	OriginCountry string
+	ExclusiveTitles []ExclusiveTitle
 }
